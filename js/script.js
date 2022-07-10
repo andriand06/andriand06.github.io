eb07a589713492877aaff4c63215e66bf7daa618
andriand06 <64359593+andriand06@users.noreply.github.com>
2022-07-10 18:46:00 +0700

    added staycation and server-staycation project

--- js/script.js
+++ js/script.js
@@ -1,9 +1,3 @@
-const jumbo = document.getElementById('jumbo')
-const thumbs = document.querySelectorAll('img-thumbnail')
-const container = document.getElementById('container')
-
-container.addEventListener('click',function(e){
-    if (e.target.className = 'img-thumbnail'){
-        jumbo.src = e.target.src;
-    }
-})
+const jumbo = document.getElementById("jumbo");
+const thumbs = document.querySelectorAll("img-thumbnail");
+const container = document.getElementById("container");
